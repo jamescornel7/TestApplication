47c21ba4e098bf8e7053768a41b5fefce156f2d8
James Cornel <jamescornel7@gmail.com>
2024-04-10 18:47:44 +0800

    Added code Challenge tasks

---
+++ TestApplication/Function.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    public class Function
+    {
+        // item 9
+        public string ToUpperCase(string val)
+        {
+            return val.ToUpper();
+        }
+
+        public int checkRange(int val)
+        {
+            try {
+                if (val >= 0 && val <= 1000)
+                {
+                    return val;
+                }
+                throw new Exception("out of range of 0 to 1000");
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+
+        public int divide(int numerator, int denominator)
+        {
+            try
+            {
+                if (denominator != 0) { 
+                    return numerator / denominator;
+                }
+                throw new Exception("0 denominator");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+            
+        }
+
+        public double squareroot(double val)
+        {
+            try
+            {
+                if (val < 0)
+                {
+                    return Math.Sqrt(val);
+                }
+                throw new Exception("negative value");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+        }
+
+        public int checkInt32(string val)
+        {
+            try
+            {
+                if (int.TryParse(val, out int value))
+                {
+                    return value;
+                }
+                throw new Exception("out of range of int32");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+        }
+
+        public string checkDateTimeFormat(string val)
+        {
+            try
+            {
+                DateTime d;
+                bool checkFormat = DateTime.TryParseExact(val, "dd/mm/yyyy",CultureInfo.InvariantCulture,DateTimeStyles.None, out d);
+                if (checkFormat)
+                {
+                    return "date format is valid: " + val;
+                }
+                throw new Exception("date format is invalid: " + val);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "";
+            }
+
+        }
+    }
+}
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using TestApplication;
+
+namespace main.application {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Function fun = new Function();
+            Console.WriteLine("input string");
+            string input = Console.ReadLine();
+            Console.WriteLine("uppercase value: " + fun.ToUpperCase(input));
+            Console.WriteLine("input number between 0 to 1000");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value: " + fun.checkRange(input2));
+            Console.WriteLine("numerator");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("denominator");
+            int den = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value: " + fun.divide(num, den));
+            Console.WriteLine("input number");
+            double sqrt = double.Parse(Console.ReadLine());
+            Console.WriteLine("value: " + fun.squareroot(num));
+            Console.WriteLine("input int32");
+            string interger = Console.ReadLine();
+            Console.WriteLine("value: " + fun.checkInt32(interger));
+            Console.WriteLine("enter date (dd/mm/yyyy)");
+            string date = Console.ReadLine();
+            Console.WriteLine("value: " + fun.checkDateTimeFormat(date));
+
+        }
+    }
+}
+
